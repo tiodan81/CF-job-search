f8a9646b1ec6dc7e81c49fad42c5467515ce98d9
Tim Mendoza <timmydoza@gmail.com>
2016-03-13 15:05:28 -0700

    finish side bar and sort functions

--- src/js/controllers/job_search_controller.js
+++ src/js/controllers/job_search_controller.js
@@ -1,5 +1,27 @@
 module.exports = function(app) {
   app.controller('jobSearchCtrl', ['$scope', 'jobData', function($scope, jobData) {
     $scope.jobs = jobData.getJobs();
+
+    $scope.stackFunction = function(job) {
+      if (!$scope.stack) {
+        return true;
+      }
+      return job[$scope.stack];
+    };
+
+    $scope.cityFunction = function(job) {
+      if (!$scope.city) {
+        return true;
+      }
+      return $scope.city === job.city;
+    };
+
+    $scope.sizeFunction = function(job) {
+      if (!$scope.size) {
+        return true;
+      }
+      return $scope.size === job.size;
+    };
+
   }]);
 };
