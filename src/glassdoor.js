fa5f199fc5a4c4e44a37fe6a492a17196e942c70
Amy Leek <amyeleek@gmail.com>
2016-03-14 11:10:00 -0700

    More handling the Glassdoor API. Very sketchy for right now

--- src/glassdoor.js
+++ src/glassdoor.js
@@ -1,6 +1,14 @@
 (function(module){
 	var glassdoor = {};
 
+	function Company(args){
+    	for (key in args) this[key] = args[key];
+  	};
+
+  	glassdoor.loadData = function(company){
+  		return new Company(company);
+  	}
+
 	//Sample API call: http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=56941&t.k=bPTsEIdyLXk&action=employers&q=Walt%20Disney%20Company
 	glassdoor.fetchData = function(companyName, callback){
 		var apiId = '56941',
@@ -11,12 +19,14 @@
 			         '&callback=?';
 
 
-		$.getJSON(apiUrl, function(result){
-   			console.log(result);
+		$.getJSON(apiUrl, function(data){
+			//may or may not want the [0] here. Without it we get an array of related companies back
+   			var company = glassdoor.loadData(data.response.employers[0]);
+   			console.log(company);
+
+   			//and call the callback, if there is one
+   			if(callback){callback();}
 		});	         
-
-
-		if(callback){callback();}
 	}
 
 	module.glassdoor = glassdoor;
