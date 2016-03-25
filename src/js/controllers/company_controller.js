a96d7a6b9530996b6113ae6e31e3c568afc99b25
Tim Mendoza <timmydoza@gmail.com>
2016-03-25 15:52:29 -0700

    finish styling for company page

--- src/js/controllers/company_controller.js
+++ src/js/controllers/company_controller.js
@@ -1,5 +1,12 @@
 module.exports = function(app) {
   app.controller('companyCtrl', ['$scope', '$routeParams', 'jobData', 'glassdoor', function($scope, $routeParams, jobData, glassdoor) {
+    $scope.stacks = {
+      javascript: 'JavaScript',
+      ios: 'iOS',
+      python: 'Python',
+      ux: 'UXE',
+      ruby: 'Ruby on Rails'
+    }
     $scope.company = jobData.getCompany($routeParams.name);
 
     $scope.getGlassdoor = function() {
